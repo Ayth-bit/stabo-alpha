71fc299136c51ba4d52a2318050f50ad9fe2bbb1
ayth-bit <slo.overwatch@gmail.com>
2025-03-02 23:33:25 +0900

    supabase接続

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import BBSCardlist from "./components/BBSCardlist";
+import BBSCardlist from "./components/BBSCardlist"; 
 
- 
-
-export default function Home() {
+export default async function Home() {
+  const response = await fetch("http://localhost:3000/api/post", {
+    cache : "no-store",
+  });
+  const BBSAllData = await response.json();
+  console.log(BBSAllData);
   return (
     <main >
       <BBSCardlist />
